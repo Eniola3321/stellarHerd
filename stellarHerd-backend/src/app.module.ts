d05cf1b0c2c9d2e72796846ac270de938b0439cb
Olawale880 <ibrahimolawale880@gmail.com>
2026-06-12 15:59:53 +0100

    issues set

--- stellarHerd-backend/src/app.module.ts
+++ stellarHerd-backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AiModule } from './ai/ai.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { RewardModule } from './reward/reward.module';
+import { FarmModule } from './farm/farm.module';
+import { LivestockModule } from './livestock/livestock.module';
+import { CooperativeModule } from './cooperative/cooperative.module';
+import { HealthModule } from './health/health.module';
+import { VaccinationModule } from './vaccination/vaccination.module';
+import { FeedLogsModule } from './feed-logs/feed-logs.module';
+import { BreedRecordModule } from './breed-record/breed-record.module';
+import { ProductivityRecordModule } from './productivity-record/productivity-record.module';
+import { MetricsModule } from './metrics/metrics.module';
+import { SyncQueueModule } from './sync-queue/sync-queue.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AiModule, PrismaModule, RewardModule, FarmModule, LivestockModule, CooperativeModule, HealthModule, VaccinationModule, FeedLogsModule, BreedRecordModule, ProductivityRecordModule, MetricsModule, SyncQueueModule],
   controllers: [AppController],
   providers: [AppService],
 })
