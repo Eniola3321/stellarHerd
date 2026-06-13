d2abcac64c0c18dbe9d7808bd6d63328e1afe047
Eniola3321 <ekanadeadeniyiabdulhafis@gmail.com>
2026-06-13 11:26:55 +0100

    feat: user controller set

--- stellarHerd-backend/src/app.module.ts
+++ stellarHerd-backend/src/app.module.ts
@@ -15,6 +15,7 @@ import { BreedRecordModule } from './breed-record/breed-record.module';
 import { ProductivityRecordModule } from './productivity-record/productivity-record.module';
 import { MetricsModule } from './metrics/metrics.module';
 import { SyncQueueModule } from './sync-queue/sync-queue.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule, AiModule, PrismaModule, RewardModule, FarmModule, LivestockModule, CooperativeModule, HealthModule, VaccinationModule, FeedLogsModule, BreedRecordModule, ProductivityRecordModule, MetricsModule, SyncQueueModule],
